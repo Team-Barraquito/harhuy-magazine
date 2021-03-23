9a69a501c0fd3a431c7dbf99d4be03680c657eb8
JoseCova <alu0101023663@ull.edu.es>
2021-03-23 00:59:40 +0000

    Created decrease button component

---
+++ src/components/DecreaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="w-10 h-10 bg-black text-white ">-</button>
+</template>
+
+<script>
+export default {
+  name: "DecreaseButton",
+  methods: {
+    decreaseQuantity () {
+
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
