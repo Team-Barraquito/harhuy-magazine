c5185e46c028bb1354549640ff582bbe956c93ff
JoseCova <alu0101023663@ull.edu.es>
2021-03-23 00:52:25 +0000

    Created increase button component

---
+++ src/components/IncreaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+<button class="w-10 h-10 bg-black text-white " >+</button>
+</template>
+
+<script>
+export default {
+  name: "IncreaseButton",
+  methods: {
+    increaseQuantity () {
+
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
