d3f2e25eab79e7e3810dc23bac6d441f6d2faae2
JoseCova <alu0101023663@ull.edu.es>
2021-03-23 01:20:41 +0000

    Created quantity selector component

---
+++ src/components/QuantitySelector.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex  w-64 h-28 bg-pink-50">
+    <decrease-button> </decrease-button>
+    <div class="w-10 h-10"> {{quantity}} </div>
+    <increase-button> </increase-button>
+  </div>
+</template>
+
+<script>
+import DecreaseButton from "@/components/DecreaseButton.vue";
+import IncreaseButton from "@/components/IncreaseButton.vue";
+
+export default {
+  name: "QuantitySelector",
+  components: {
+    DecreaseButton,
+    IncreaseButton,
+  },
+
+  data () {
+    return {
+      quantity: 0,
+    };
+  },
+
+};
+</script>
+
+<style>
+
+</style>
+
+DecreaseButton
