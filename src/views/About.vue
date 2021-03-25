a695938457f9ae01a4f2d130e8482fd32a2f641f
JoseCova <alu0101023663@ull.edu.es>
2021-03-25 13:16:52 +0000

    modified view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <top-bar> </top-bar>
     <quantity-selector> </quantity-selector>
     <side-menu> </side-menu>
   </div>
 </template>
 
 <script>
-import QuantitySelector from "@/components/QuantitySelector.vue";
-import SideMenu from "@/components/SideMenu.vue";
+import TopBar from "@/components/TopBar.vue";
 
 export default {
   name: "About",
   components: {
-    QuantitySelector,
-    SideMenu,
+    TopBar,
   },
 };
 </script>
