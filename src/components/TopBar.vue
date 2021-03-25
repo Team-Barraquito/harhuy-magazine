e70d95516369816f59644b57149e940427e89b2e
JoseCova <alu0101023663@ull.edu.es>
2021-03-25 13:16:19 +0000

    Styling component

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-
+  <div class=" flex justify-between w-screen h-60 bg-gray-400">
+    <div class="w-96 h-full bg-pink-400 flex flex-col">
+      <div class="w-28 h-full bg-red-500"></div>
+    </div>
+    <div class="w-96 h-full bg-blue-400"></div>
+    <div class="w-96 h-full bg-green-400 flex flex-col items-center">
+      <div class="w-full h-44 bg-yellow-400"></div>
+    </div>
   </div>
 </template>
 
