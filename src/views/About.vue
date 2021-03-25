0bf3935caa3937bf6c2f3040a59241bd06f54fa9
JoseCova <alu0101023663@ull.edu.es>
2021-03-25 02:06:57 +0000

    Checking components

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <quantity-selector> </quantity-selector>
+    <side-menu> </side-menu>
   </div>
 </template>
 
 <script>
 import QuantitySelector from "@/components/QuantitySelector.vue";
+import SideMenu from "@/components/SideMenu.vue";
 
 export default {
   name: "About",
   components: {
     QuantitySelector,
+    SideMenu,
   },
 };
 </script>
