45dc04002fef2f24f071a905749748b4aedf9d25
JoseCova <alu0101023663@ull.edu.es>
2021-03-25 02:06:03 +0000

    Created side menu

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="w-32 flex flex-col items-center">
+  <div class="uppercase">ilustración</div>
+  <div class="vertical-line"></div>
+  <div class="uppercase">fotografía</div>
+  <div class="vertical-line"></div>
+  <div class="uppercase">música</div>
+  <div class="vertical-line"></div>
+  <div class="uppercase">vídeo</div>
+  <div class="vertical-line"></div>
+  <div class="uppercase">diseño</div>
+  <div class="vertical-line last"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SideMenu",
+};
+</script>
+
+<style>
+  .vertical-line {
+    border-left: 1px solid black;
+    height: 50px;
+  }
+
+  .last {
+    height: 200px;
+  }
+</style>
