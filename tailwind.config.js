451b9e7d363d8279349b6a995895ff1f3131ada3
JoseCova <alu0101023663@ull.edu.es>
2021-03-26 23:10:29 +0000

    Added new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // Or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        harhuy: "#FFEC02",
+      },
       borderWidth:
       {
         1: "1px",
