2f0e21b7c71aed185b42aac178b5928b41c54db1
JoseCova <alu0101023663@ull.edu.es>
2021-03-27 18:57:53 +0000

    Added links to new views

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/menu"> Menu </router-link>
+      <router-link to="/menu"> Menu </router-link> |
+      <router-link to="/aboutUs"> About Us </router-link> |
+      <router-link to="/contactUs"> Contact Us </router-link>
     </div>
     <router-view/>
   </div>
