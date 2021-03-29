dee1d9fce85e426447867eb9519ea60f08555508
JoseCova <alu0101023663@ull.edu.es>
2021-03-30 00:45:13 +0100

    Created component

---
+++ src/components/StoreItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="relative text-black">
+    <img :src=item.photos.front alt="Camisa Harhuy" srcset="">
+    <div class="absolute bottom-2 left-2 text-xs"> {{ item.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StoreItem",
+  props: {
+    item: {
+      name: String,
+      isInStock: Boolean,
+      rating: Number,
+      price: Number,
+      photos: Object,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
