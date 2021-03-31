2e425d83b6efa8ce71f4adcf7620b81dbdc10cb2
JoseCova <alu0101023663@ull.edu.es>
2021-03-31 01:36:59 +0100

    Created store view

---
+++ src/views/HarhuyStore.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="store">
+    <TopBar> </TopBar>
+    <SideMenu> </SideMenu>
+    <StoreItem v-for="(item, index) of this.items" :key="index" :item="item" > </StoreItem>
+  </div>
+</template>
+
+<script>
+import StoreItem from "@/components/StoreItem.vue";
+import TopBar from "@/components/TopBar.vue";
+import SideMenu from "@/components/SideMenu.vue";
+
+export default {
+  name: "HarhuyStore",
+  components: {
+    StoreItem,
+    TopBar,
+    SideMenu,
+  },
+
+  data () {
+    return {
+      items: [],
+    };
+  },
+
+  methods: {
+    setItems () {
+      const storeItems = require("../assets/database/store-items.json");
+      this.items = storeItems.items;
+    },
+  },
+
+  mounted () {
+    this.setItems();
+  },
+};
+
+</script>
+
+<style scoped>
+
+</style>
