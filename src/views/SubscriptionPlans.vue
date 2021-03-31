8a1577b2e74a673b7a7742d900b5b661e4b69c75
JoseCova <alu0101023663@ull.edu.es>
2021-03-31 01:37:19 +0100

    Created subscription plans view

---
+++ src/views/SubscriptionPlans.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="subscription">
+    <TopBar> </TopBar>
+    <SideMenu> </SideMenu>
+  </div>
+</template>
+
+<script>
+import TopBar from "@/components/TopBar.vue";
+import SideMenu from "@/components/SideMenu.vue";
+export default {
+  name: "SubscriptionPlans",
+  components: {
+    TopBar,
+    SideMenu,
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
