a5190583604d71561f25a41af15f8e5399452a87
JoseCova <alu0101023663@ull.edu.es>
2021-04-01 14:05:55 +0100

    Refactoring view  to use vuex

--- src/views/HarhuyStore.vue
+++ src/views/HarhuyStore.vue
@@ -6,11 +6,7 @@
         <SideMenu> </SideMenu>
       </div>
       <div class="content flex flex-col">
-      <!-- Aqui nuevo compo -->
-        <StoreFeaturedItem :info=info> </StoreFeaturedItem>
-        <div class="grid grid-cols-3 grid-rows-2 gap-x-5 gap-y-3">
-          <StoreItem v-for="(item, index) in items" :key=index :item=item > </StoreItem>
-        </div>
+        <StoreFeaturedItem> </StoreFeaturedItem>
       </div>
     </div>
   </div>
@@ -19,7 +15,7 @@
 <script>
 import TopBar from "@/components/TopBar.vue";
 import SideMenu from "@/components/SideMenu.vue";
-import StoreItem from "@/components/StoreItem.vue";
+// Import StoreItem from "@/components/StoreItem.vue";
 import StoreFeaturedItem from "@/components/StoreFeaturedItem.vue";
 
 export default {
@@ -28,47 +24,6 @@ export default {
     TopBar,
     SideMenu,
     StoreFeaturedItem,
-    StoreItem,
-  },
-
-  data () {
-    return {
-      items: [],
-      info: {},
-    };
-  },
-
-  methods: {
-    // TODO cambiar nombre function
-    setItems () {
-      /*
-       *  This function gets all the store
-       *  items and the featured one
-       *  depending on the month we are
-       */
-      const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "November",
-        "December",
-      ];
-      const monthIndex = new Date().getMonth();
-      const currentMonth = months[monthIndex];
-      const storeItems = require("../assets/database/store-items.json");
-      this.items = storeItems.items;
-      this.info = storeItems.featuredItems[`${currentMonth}`];
-    },
-  },
-
-  created () {
-    this.setItems();
   },
 };
 
