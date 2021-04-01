be3926cc8a3ae9f3d8fa1872f414d79357937919
JoseCova <alu0101023663@ull.edu.es>
2021-04-01 02:23:19 +0100

    Added components

--- src/views/HarhuyStore.vue
+++ src/views/HarhuyStore.vue
@@ -1,38 +1,73 @@
 <template>
   <div class="store">
     <TopBar> </TopBar>
-    <SideMenu> </SideMenu>
-    <StoreItem v-for="(item, index) of this.items" :key="index" :item="item" > </StoreItem>
+    <div class="container flex">
+      <div class="nav-container">
+        <SideMenu> </SideMenu>
+      </div>
+      <div class="content flex flex-col">
+      <!-- Aqui nuevo compo -->
+        <StoreFeaturedItem :info=info> </StoreFeaturedItem>
+        <div class="grid grid-cols-3 grid-rows-2 gap-x-5 gap-y-3">
+          <StoreItem v-for="(item, index) in items" :key=index :item=item > </StoreItem>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import StoreItem from "@/components/StoreItem.vue";
 import TopBar from "@/components/TopBar.vue";
 import SideMenu from "@/components/SideMenu.vue";
+import StoreItem from "@/components/StoreItem.vue";
+import StoreFeaturedItem from "@/components/StoreFeaturedItem.vue";
 
 export default {
   name: "HarhuyStore",
   components: {
-    StoreItem,
     TopBar,
     SideMenu,
+    StoreFeaturedItem,
+    StoreItem,
   },
 
   data () {
     return {
       items: [],
+      info: {},
     };
   },
 
   methods: {
+    // TODO cambiar nombre function
     setItems () {
+      /*
+       *  This function gets all the store
+       *  items and the featured one
+       *  depending on the month we are
+       */
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "November",
+        "December",
+      ];
+      const monthIndex = new Date().getMonth();
+      const currentMonth = months[monthIndex];
       const storeItems = require("../assets/database/store-items.json");
       this.items = storeItems.items;
+      this.info = storeItems.featuredItems[`${currentMonth}`];
     },
   },
 
-  mounted () {
+  created () {
     this.setItems();
   },
 };
