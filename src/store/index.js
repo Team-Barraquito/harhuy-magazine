66d51d341f4ba279e6059b31bcaed22e7626c5df
JoseCova <alu0101023663@ull.edu.es>
2021-04-01 14:03:38 +0100

    Added states

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+const productDatabase = require("../assets/database/store-items.json");
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    cart: [],
+    items: productDatabase.items,
+    featuredItems: productDatabase.featuredItems,
   },
+
   mutations: {
   },
   actions: {
