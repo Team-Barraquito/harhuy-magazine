bd0be7c9a7b5e10ec74a7fb71ae15b3d95e57e6c
JoseCova <alu0101023663@ull.edu.es>
2021-04-01 14:04:17 +0100

    modified component now using vuex to get data

--- src/components/StoreFeaturedItem.vue
+++ src/components/StoreFeaturedItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex ml-20">
-    <img :src="getSource" :alt="info.name" class="w-116 h-116">
+    <img :src=getPhoto() class="w-116 h-116">
     <div class="info-container w-116 h-116">
       <div class="h-1/3 flex items-end">
         <h1 class="text-3xl p-1 font-bold"> Producto destacado </h1>
       </div>
-      <p class="mt-4 p-1 text-2xl text-center"> {{ info.description }}</p>
+      <p class="mt-4 p-1 text-2xl text-center"> {{ itemData.description }}  </p>
     </div>
   </div>
 </template>
@@ -13,23 +13,41 @@
 <script>
 export default {
   name: "StoreFeaturedItem",
-  props: {
-    info: {
-      type: Object,
-      name: String,
-      isInStock: Boolean,
-      rating: Number,
-      price: Number,
-      photo: String,
-      description: String,
-      required: true,
-    },
+  data () {
+    return {
+      itemData: {},
+    };
   },
-  computed: {
-    getSource () {
-      const photoPath = this.$props.info.photo;
+  methods: {
+    getMonth () {
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const currentMonthIndex = new Date().getUTCMonth();
+      return months[currentMonthIndex];
+    },
+    setData () {
+      const currentMonth = this.getMonth();
+      this.itemData = this.$store.state.featuredItems[`${currentMonth}`];
+    },
+    getPhoto () {
+      const photoPath = this.itemData.photo;
       return require("../assets/images" + photoPath);
     },
+  },
+  beforeMount () {
+    this.setData();
   },
 };
 </script>
