2675132f71b83c304f04fccab7b8d6d18d5385a5
JoseCova <alu0101023663@ull.edu.es>
2021-04-01 02:15:12 +0100

    Added more custom width and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
       },
       height: {
         112: "28rem",
+        116: "28.125rem",
         128: "31rem",
+        large: "31.25rem",
+      },
+      width: {
+        116: "28.125rem",
+        large: "31.25rem",
       },
     },
   },
