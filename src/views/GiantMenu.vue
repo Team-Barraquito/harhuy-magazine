ca4080bd67939e6a6002873680c996ba3b7d1008
JoseCova <alu0101023663@ull.edu.es>
2021-04-03 16:21:23 +0100

    changed button

--- src/views/GiantMenu.vue
+++ src/views/GiantMenu.vue
@@ -1,15 +1,16 @@
 <template>
 <!-- TODO ajustar tamaño de pantalla -->
-  <div class="w-screen h-screen bg-harhuy menu">
+  <div class="w-screen h-screen bg-harhuy menu"  @mousemove="moveCustomCursor">
     <CircularCursor id="customCursor"> </CircularCursor>
     <div class="top-zone overflow-hidden w-screen h-28 grid grid-rows-1 grid-cols-3">
       <!-- fixear -->
       <div class="flex justify-start items-start">
-        <button class="cross-button">
+        <button @click=goToBefore>
           <svg width="44" height="55" viewBox="0 0 44 55" fill="none" xmlns="http://www.w3.org/2000/svg">
             <line y1="-3.5" x2="55.5613" y2="-3.5" transform="matrix(0.699963 0.709914 -0.704254 0.714266 0 7.21875)" stroke="black" stroke-width="7"/>
             <line y1="-3.5" x2="55.5613" y2="-3.5" transform="matrix(-0.699963 0.709914 0.704254 0.714266 43.8906 7.33984)" stroke="black" stroke-width="7"/>
           </svg>
+          <div class="text-black uppercase font-bold rotate menu "> close</div>
         </button>
       </div>
       <div class="flex items-center justify-center">
@@ -53,16 +54,21 @@
 
 <script>
 import CircularCursor from "@/components/CircularCursor.vue";
+
 export default {
   name: "GiantMenu",
   components: {
     CircularCursor,
   },
+
   methods: {
     moveCustomCursor (event) {
       const cursor = this.$el.querySelector("#customCursor");
       cursor.style.top = event.pageY + "px";
       cursor.style.left = event.pageX + "px";
+    },
+    goToBefore () {
+      this.$router.go(-1);
     },
   },
 
@@ -84,6 +90,10 @@ export default {
 #arrow {
   height: 1.5rem;
   width: 1.5rem;
+}
+
+.rotate {
+  transform: rotate(270deg);
 }
  /* Falta rellenar on hover */
 </style>
