a665545224f296c20fc201a7e0cb2736826de629
JoseCova <alu0101023663@ull.edu.es>
2021-04-04 12:31:14 +0100

    removed components from view

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -34,19 +34,11 @@
 </template>
 
 <script>
-import TopBar from "@/components/TopBar.vue";
-import SideMenu from "@/components/SideMenu.vue";
-
 export default {
   name: "AboutUs",
-  components: {
-    TopBar,
-    SideMenu,
-  },
 };
 </script>
 
 <style scoped>
 
 </style>
-
