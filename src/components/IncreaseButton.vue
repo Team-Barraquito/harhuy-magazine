8c028bf9b54e59655f207773a3131160748a50a7
JoseCova <alu0101023663@ull.edu.es>
2021-04-04 12:18:28 +0100

    Added mapActions

--- src/components/IncreaseButton.vue
+++ src/components/IncreaseButton.vue
@@ -1,14 +1,13 @@
 <template>
-<button class="w-10 h-10 bg-black text-white " >+</button>
+<button @click=increaseQuantity class="w-10 h-10 bg-black text-white " >+</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "IncreaseButton",
   methods: {
-    increaseQuantity () {
-
-    },
+    ...mapActions(["increaseQuantity"]),
   },
 };
 </script>
