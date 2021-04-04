c0f68c4d9f9a7ae1650097a6701529b4cc180534
JoseCova <alu0101023663@ull.edu.es>
2021-04-04 12:32:10 +0100

    Created component

---
+++ src/components/StoreBadge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> Carrito</div>
+</template>
+
+<script>
+export default {
+  name: "StoreBadge",
+  props: {
+    itemCount: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
