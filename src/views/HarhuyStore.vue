e9c685ebed7a139f64f766a4b68e59b54f32743f
Irina Leston <irinaleston@gmail.com>
2021-04-07 15:09:33 +0100

    Plan Card object and home

--- src/views/HarhuyStore.vue
+++ src/views/HarhuyStore.vue
@@ -5,7 +5,7 @@
       <div class="nav-container">
         <SideMenu> </SideMenu>
       </div>
-      <div class="content flex flex-col">
+      <div class="content flex flex-col items-end ">
         <StoreFeaturedItem> </StoreFeaturedItem>
         <div class="grid grid-rows-2 grid-cols-3">
           <StoreItem v-for="(item, index) in items" :key="index" :item="item"> </StoreItem>
--- src/views/SubscriptionPlans.vue
+++ src/views/SubscriptionPlans.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="subscription">
     <TopBar> </TopBar>
-    <SideMenu> </SideMenu>
+    <div class="container flex w-screen">
+      <SideMenu> </SideMenu>
+      <div class="flex justify-center items-center w-70%">
+        <PlanCard> </PlanCard>
+        <PlanCard> </PlanCard>
+        <PlanCard> </PlanCard>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import TopBar from "@/components/TopBar.vue";
 import SideMenu from "@/components/SideMenu.vue";
+import PlanCard from "@/components/PlanCard.vue";
 export default {
   name: "SubscriptionPlans",
   components: {
     TopBar,
     SideMenu,
+    PlanCard,
   },
 
 };
