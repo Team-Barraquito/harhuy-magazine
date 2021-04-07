7898a9a62347c28e757a67bfe8954429d6f7fe97
JoseCova <alu0101023663@ull.edu.es>
2021-04-07 17:00:55 +0100

    Added css hover effect

--- src/components/StoreItem/StoreItem.vue
+++ src/components/StoreItem/StoreItem.vue
@@ -1,7 +1,10 @@
 <template>
-  <div @click="goToProductSheet"  class="relative text-black">
+  <div @click="goToProductSheet"  class="prueba relative text-black">
     <img :src="getSource" :alt=item.name>
     <div class="absolute bottom-2 left-2 text-xs"> {{ item.name }}</div>
+    <div class="overlay absolute bottom-0 left-0 right-0 bg-blue-300 overflow-hidden w-full h-0">
+      <div class="text whitespace-nowrap text-white overflow-hidden absolute text-xl top-1/2 left-1/2">prueba</div>
+    </div>
   </div>
 </template>
 
@@ -34,5 +37,15 @@ export default {
 </script>
 
 <style scoped>
+.overlay{
+  transition: .5s ease;
+}
+.text {
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%,-50%);
+}
 
+.prueba:hover .overlay{
+  height: 100%;
+}
 </style>
