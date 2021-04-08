cb1c411d5d94fb47374dd2742d90ccb12a00d1d4
JoseCova <alu0101023663@ull.edu.es>
2021-04-08 15:47:23 +0100

    Added grid gap

--- src/views/HarhuyStore.vue
+++ src/views/HarhuyStore.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="content flex flex-col">
         <StoreFeaturedItem> </StoreFeaturedItem>
-        <div class="grid grid-rows-2 grid-cols-3">
+        <div class="grid grid-rows-2 grid-cols-3 gap-4">
           <StoreItem v-for="(item, index) in items" :key="index" :item="item"> </StoreItem>
         </div>
       </div>
