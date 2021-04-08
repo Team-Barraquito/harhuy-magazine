5f022a8ba853985ccf7274a27eb789e9640650b7
JoseCova <alu0101023663@ull.edu.es>
2021-04-08 16:00:11 +0100

    changed import

--- src/views/SubscriptionPlans.vue
+++ src/views/SubscriptionPlans.vue
@@ -8,6 +8,7 @@
 <script>
 import TopBar from "@/components/TopBar/TopBar.vue";
 import SideMenu from "@/components/SideMenu/SideMenu.vue";
+
 export default {
   name: "SubscriptionPlans",
   components: {
