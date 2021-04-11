5de4cc8d16f1cb32fd479b28076d529074b57625
JoseCova <alu0101023663@ull.edu.es>
2021-04-11 17:12:55 +0100

    now view gets the selected artist data

--- src/views/Illustrator.vue
+++ src/views/Illustrator.vue
@@ -1,10 +1,28 @@
 <template>
-  <div> </div>
+  <div></div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "Illustrator",
+  data () {
+    return {
+      illustrator: {},
+    };
+  },
+  computed: {
+    ...mapGetters(["illustrators"]),
+  },
+  methods: {
+    getDesigner () {
+      this.illustrator = this.illustrators(this.$route.params.name);
+    },
+  },
+  beforeMount () {
+    this.getDesigner();
+  },
 };
 </script>
 
