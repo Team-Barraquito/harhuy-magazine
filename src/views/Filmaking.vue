d3bf25e6d542de7bbd704bf5e16603df0856446c
JoseCova <alu0101023663@ull.edu.es>
2021-04-14 12:54:22 +0100

    renamed from video to filmaking

---
+++ src/views/Filmaking.vue
@@ -0,0 +1,71 @@
+<template>
+ <div class="illustration overflow-x-hidden">
+    <TopBar> </TopBar>
+    <div class="w-screen flex overflow-x-hidden">
+      <div class="nav-container overflow-x-hidden">
+        <SideMenu> </SideMenu>
+      </div>
+      <!-- End of top side -->
+        <div class="w-full flex justify-evenly px-4 overflow-x-hidden">
+          <div class="column w-96 h-full grid gap-3 justify-items-center">
+            <ShowcaseArtist v-for="(artist,index) in firstThree" :key="index" :artistData=artist :path="path"/>
+          </div>
+          <div class="column w-96 h-full grid gap-12 justify-items-center pt-14">
+            <ShowcaseArtist v-for="(artist,index) in secondThree" :key="index" :artistData=artist :path="path"/>
+          </div>
+          <div class="column w-96 h-full grid gap-3 justify-items-center">
+           <ShowcaseArtist v-for="(artist,index) in thirdThree" :key="index" :artistData=artist :path="path"/>
+          </div>
+       </div>
+    </div>
+    <div class="footer-container overflow-x-hidden">
+      <Footer> </Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopBar from "@/components/TopBar/TopBar.vue";
+import SideMenu from "@/components/SideMenu/SideMenu.vue";
+import ShowcaseArtist from "@/components/ShowcaseArtist/ShowcaseArtist.vue";
+import Footer from "@/components/Footer/Footer.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "Filmaking",
+  data () {
+    return {
+      firstThree: [],
+      secondThree: [],
+      thirdThree: [],
+    };
+  },
+  components: {
+    TopBar,
+    SideMenu,
+    ShowcaseArtist,
+    Footer,
+  },
+  computed: {
+    ...mapState(["videoArtists"]),
+    path () {
+      const path = this.$route.path.replace("/", "");
+      return path;
+    },
+  },
+  methods: {
+    setArraysContent () {
+      this.firstThree = this.videoArtists.slice(0, 3);
+      this.secondThree = this.videoArtists.slice(3, 6);
+      this.thirdThree = this.videoArtists.slice(6);
+    },
+  },
+  beforeMount () {
+    this.setArraysContent();
+  },
+};
+</script>
+
+<style>
+
+</style>
