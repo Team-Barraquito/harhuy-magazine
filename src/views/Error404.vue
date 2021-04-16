1a89a756f034fb4466915207001a26c072be5f2e
JoseCova <alu0101023663@ull.edu.es>
2021-04-16 09:49:50 +0100

    added footer

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="global bg-harhuy h-screen">
+<div class="error h-screen">
       <!-- TopBar + SideMenu --->
     <TopBar> </TopBar>
-    <div class="w-screen flex">
+    <div class="w-screen h-4.5/6 flex bg-harhuy">
       <div class="nav-container">
         <SideMenu> </SideMenu>
       </div>
     <!-- End of TopBar + SideMenu -->
 
     <div class="flex items-center justify-center w-full h-5/6 text-left text-black">
-        <div >
+      <div>
         <p class="text-3xl font-extrabold text-right">Fck*!</p>
       <p>/ ERROR 404 /</p>
       </div>
@@ -18,9 +18,9 @@
         <p>The page you're looking for</p>
         <p>is now beyond our reach</p>
       </div>
+      </div>
     </div>
-  </div>
-  <div class="footer-container h-1/6"> <Footer> </Footer> </div>
+  <div class="footer-container"> <Footer> </Footer> </div>
 </div>
 </template>
 
