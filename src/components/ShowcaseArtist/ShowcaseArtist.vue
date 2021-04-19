fa91b47590d1f426a3bf988989962ecb855f9d3e
JoseCova <alu0101023663@ull.edu.es>
2021-04-19 10:27:19 +0100

    made responsive

--- src/components/ShowcaseArtist/ShowcaseArtist.vue
+++ src/components/ShowcaseArtist/ShowcaseArtist.vue
@@ -2,9 +2,9 @@
   <div class="w-full h-full flex flex-col justify-center items-center">
     <div @click="goToArtist" class="w-11/12 h-11/12 flex flex-col justify-center items-center">
       <img class="w-11/12 h-auto" :src=getSource :alt=artistData.name>
-      <div class="text-left w-full pl-4">
-        <p class="font-semibold tracking-wide">{{artistData.name}}</p>
-        <p class="text-gray-600">{{artistData.socialMedia}}</p>
+      <div class="text-left w-full pl-4 tablet:text-center desktop:text-left">
+        <p class="font-semibold tracking-wide tablet:text-lg laptop:text-xl desktop:text-base xl:text-lg">{{artistData.name}}</p>
+        <p class="text-gray-600 laptop:text-lg desktop:text-sm xl:text-base">{{artistData.socialMedia}}</p>
       </div>
     </div>
   </div>
