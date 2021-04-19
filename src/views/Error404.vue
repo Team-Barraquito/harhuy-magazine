b11712d1cadad0b139da1be00c99021dbd17ce5d
JoseCova <alu0101023663@ull.edu.es>
2021-04-19 10:24:18 +0100

    made images bigger

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -2,21 +2,21 @@
 <div class="error h-screen">
       <!-- TopBar + SideMenu --->
     <TopBar> </TopBar>
-    <div class="w-screen h-4.5/6 flex bg-harhuy">
+    <div class="w-screen flex bg-harhuy">
       <div class="nav-container">
         <SideMenu> </SideMenu>
       </div>
     <!-- End of TopBar + SideMenu -->
 
-    <div class="flex items-center justify-center w-full h-5/6 text-left text-black">
-      <div>
-        <p class="text-3xl font-extrabold text-right">Fck*!</p>
-      <p>/ ERROR 404 /</p>
+    <div class="flex items-start justify-center w-full text-left text-black">
+      <div class="pt-48 -ml-20">
+        <p class="text-5xl font-extrabold text-right">Fck*!</p>
+        <p class="text-2xl">/ ERROR 404 /</p>
       </div>
       <img src="../assets/images/error404/grave.svg"/>
-      <div class="error">
-        <p>The page you're looking for</p>
-        <p>is now beyond our reach</p>
+      <div class="error pt-52 text-lg">
+        <p>¡Lo sentimos! La página que estás</p>
+        <p>buscando no existe</p>
       </div>
       </div>
     </div>
